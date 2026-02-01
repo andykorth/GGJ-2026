05ca942cc47ae7cbb2acb3a848c7cd772cfdbb47
Andy Korth <andy.korth@gmail.com>
2026-01-31 22:03:58 -0600

    Better character tracking and limit y of player

--- Scripts/TheCafe.gd
+++ Scripts/TheCafe.gd
@@ -1,5 +1,12 @@
 class_name TheCafe
 extends Node2D
 
+@onready var lenaNPC: CafeNPC = $NPCNodeLena
+@onready var eastonNPC: CafeNPC = $NPCNodeEaston
+@onready var youaNPC: CafeNPC = $NPCNodeYoua
+
 func _ready() -> void:
 	y_sort_enabled = true
+	GlobalGameVariables.lenaNPC = lenaNPC
+	GlobalGameVariables.eastonNPC = eastonNPC
+	GlobalGameVariables.youaNPC = youaNPC
