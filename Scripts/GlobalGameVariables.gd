1f4d42e398aa42c44bd92090cfd4a9890c8fb7f3
Josh Anthony <jantho1990@gmail.com>
2026-02-01 11:16:23 -0600

    tabs vs spaces sucks

--- Scripts/GlobalGameVariables.gd
+++ Scripts/GlobalGameVariables.gd
@@ -4,86 +4,86 @@ extends Node
 var currentCharacter: CharacterAttributes ## The dialogue's current character attributes. This should be set whenever starting a timeline.
 var playerControlsActive := true
 var stepsTakenInDialog := 0
-var cam : CafeCamera
-var player : CafeCharacter
+var cam: CafeCamera
+var player: CafeCharacter
 
 # it is helpful to have fixed references to each character
 # so we can reference them when swapping characters
-var lenaNPC : CafeNPC
-var eastonNPC : CafeNPC
-var youaNPC : CafeNPC
-var jesterNPC : CafeNPC
+var lenaNPC: CafeNPC
+var eastonNPC: CafeNPC
+var youaNPC: CafeNPC
+var jesterNPC: CafeNPC
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  pass
+	pass
 
 func activatePlayerControls():
-  playerControlsActive = true
-  pass
+	playerControlsActive = true
+	pass
   
 func deactivatePlayerControls():
-  playerControlsActive = false
-  pass
+	playerControlsActive = false
+	pass
 
-func ChangePlayerCharacter(newCharacter : CharacterAttributes):
-  print("Changing character to " + newCharacter.character_name)
-  player.ChangeCharacter(newCharacter)
+func ChangePlayerCharacter(newCharacter: CharacterAttributes):
+	print("Changing character to " + newCharacter.character_name)
+	player.ChangeCharacter(newCharacter)
 
 func ChangePlayerCharacterToLena():
-  ChangePlayerCharacter(lenaNPC.currentChar)
+	ChangePlayerCharacter(lenaNPC.currentChar)
   
 func ChangePlayerCharacterToEaston():
-  ChangePlayerCharacter(eastonNPC.currentChar)
+	ChangePlayerCharacter(eastonNPC.currentChar)
   
 func ChangePlayerCharacterToYoua():
-  ChangePlayerCharacter(youaNPC.currentChar)
+	ChangePlayerCharacter(youaNPC.currentChar)
 
-func MoveLenaNPCTo(x : float, y := 0.0):
-  lenaNPC.SetDestination(Vector2(x,y))
-func MoveEastonNPCTo(x : float, y := 0.0):
-  eastonNPC.SetDestination(Vector2(x,y))
-func MoveYouaNPCTo(x : float, y := 0.0):
-  youaNPC.SetDestination(Vector2(x,y))
+func MoveLenaNPCTo(x: float, y := 0.0):
+	lenaNPC.SetDestination(Vector2(x, y))
+func MoveEastonNPCTo(x: float, y := 0.0):
+	eastonNPC.SetDestination(Vector2(x, y))
+func MoveYouaNPCTo(x: float, y := 0.0):
+	youaNPC.SetDestination(Vector2(x, y))
 func MoveJesterNPCTo(x: float, y := 0.0):
-  jesterNPC.SetDestination(Vector2(x,y))
+	jesterNPC.SetDestination(Vector2(x, y))
 
 
 func incrementDialogStepsTaken():
-  stepsTakenInDialog += 1
-  print(str(stepsTakenInDialog))
-  pass
+	stepsTakenInDialog += 1
+	print(str(stepsTakenInDialog))
+	pass
 
-func animateCamera(x : float):
-  print("Starting camera animation: x => " + str(x))
-  cam.ScriptedMove(x, 0)
-  
+func animateCamera(x: float):
+	print("Starting camera animation: x => " + str(x))
+	cam.ScriptedMove(x, 0)
+
 func hideCharacter(character_name: String) -> void:
-  # Forgive me, Uncle Bob.
-  if lenaNPC and lenaNPC.currentChar.character_name == character_name:
-    lenaNPC.HideCharacter()
-  elif youaNPC and youaNPC.currentChar.character_name == character_name:
-    youaNPC.HideCharacter()
-  elif eastonNPC and eastonNPC.currentChar.character_name == character_name:
-    eastonNPC.HideCharacter()
-  elif jesterNPC and jesterNPC.currentChar.character_name == character_name:
-    jesterNPC.HideCharacter()
-  else:
-      push_error('Character "%s" not found, aborting.' % [character_name])
+	# Forgive me, Uncle Bob.
+	if lenaNPC and lenaNPC.currentChar.character_name == character_name:
+		lenaNPC.HideCharacter()
+	elif youaNPC and youaNPC.currentChar.character_name == character_name:
+		youaNPC.HideCharacter()
+	elif eastonNPC and eastonNPC.currentChar.character_name == character_name:
+		eastonNPC.HideCharacter()
+	elif jesterNPC and jesterNPC.currentChar.character_name == character_name:
+		jesterNPC.HideCharacter()
+	else:
+		push_error('Character %s not found, aborting.' % [character_name])
 
 func showCharacter(character_name: String) -> void:
-  # Forgive me, Uncle Bob.
-  if lenaNPC and lenaNPC.currentChar.character_name == character_name:
-    lenaNPC.ShowCharacter()
-  elif youaNPC and youaNPC.currentChar.character_name == character_name:
-    youaNPC.ShowCharacter()
-  elif eastonNPC and eastonNPC.currentChar.character_name == character_name:
-    eastonNPC.ShowCharacter()
-  elif jesterNPC and jesterNPC.currentChar.character_name == character_name:
-    jesterNPC.ShowCharacter()
-  else:
-      push_error('Character "%s" not found, aborting.' % [character_name])
+	# Forgive me, Uncle Bob.
+	if lenaNPC and lenaNPC.currentChar.character_name == character_name:
+		lenaNPC.ShowCharacter()
+	elif youaNPC and youaNPC.currentChar.character_name == character_name:
+		youaNPC.ShowCharacter()
+	elif eastonNPC and eastonNPC.currentChar.character_name == character_name:
+		eastonNPC.ShowCharacter()
+	elif jesterNPC and jesterNPC.currentChar.character_name == character_name:
+		jesterNPC.ShowCharacter()
+	else:
+		push_error('Character %s not found, aborting.' % [character_name])
