09a2bab374af8af837a67a7e83f4ee8b62a1c443
Andy Korth <andy.korth@gmail.com>
2026-01-31 18:15:25 -0600

    Space to chat, some y sorting, other fixes.

--- Scripts/CafeCharacter.gd
+++ Scripts/CafeCharacter.gd
@@ -5,12 +5,16 @@ extends Node2D
 # Might be scripted to move aroudn, but maybe we allow free movement
 # once the mask takes control of a character.
 
+const CHAT_RANGE = 60
+
 @export var speed:float = 120.0
 @export var currentChar : CharacterAttributes
 @export var cafe : Node
+
 var wiggleAmplitude:float = 1.0
 var destination: Vector2
 @onready var bodySprite: Sprite2D = $CharacterWorldMapSprite
+@onready var chatIcon: Sprite2D = $ChatIcon
 
 var blockedInput: bool = false;
 
@@ -20,6 +24,8 @@ func _ready() -> void:
 	destination = position
 	SetUpCharacter()
 	Dialogic.timeline_ended.connect(DialogEnded)
+	chatIcon.visible = false
+
 
 func DialogEnded():
 	blockedInput = true;
@@ -42,6 +48,8 @@ func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if(input_direction.length_squared() != 0):
 		velocity = input_direction * speed
+		# moving with keyboard cancels scripted moves.
+		destination = position
 	else:
 		# if there's no input, use the scripted destination:
 		var deltaPos = destination - position
@@ -55,22 +63,27 @@ func _process(delta: float) -> void:
 	var x = cos(Time.get_ticks_msec() / 80.0)
 	bodySprite.rotation = deg_to_rad(x * 15.0 * normalizedSpeed) 
 	bodySprite.scale = Vector2(1, cos(Time.get_ticks_msec() / 120.0) * 0.15 * normalizedSpeed + 1.0)
-	
+		
 	# apply position but need to handle collisions.
 	position = position + velocity * delta
 	
-	CheckForNewChats()
-	
-	pass
+	# need to draw a little chat icon if we are in chat range.
+	chatIcon.visible = CheckChatRange() != null
 
-# you should NOTICE this cool method
-func CheckForNewChats():
-	
+
+func CheckChatRange() -> CharacterAttributes:
 	for childNode in cafe.get_children():
 		if childNode is CafeNPC:
 			var delta = position - childNode.position
-			if delta.length() < 30:
-				StartDialogWith(childNode.currentChar)
+			if delta.length() < CHAT_RANGE:
+				return childNode.currentChar
+	return null
+
+# you should NOTICE this cool method
+func CheckForNewChats():
+	var chatter = CheckChatRange()
+	if chatter != null:
+		StartDialogWith(chatter)
 	
 func StartDialogWith(character : CharacterAttributes):
 	# stop any additional motion
@@ -79,6 +92,7 @@ func StartDialogWith(character : CharacterAttributes):
 	print('start dialog with ' + character.character_name);
 	print(character.intro_timeline)
 	DialogicConnector.startDialogue(character.intro_timeline, character)
+	blockedInput = true
 
 func SetDestination(newDest : Vector2):
 	if GlobalGameVariables.playerControlsActive && !blockedInput:
@@ -89,3 +103,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("mouse_left"):
 		# just for testing
 		SetDestination(get_global_mouse_position())
+
+	if Input.is_action_just_pressed("talkaction"):
+		# just for testing
+		CheckForNewChats()
+		
+		
---
+++ Scripts/TheCafe.gd
@@ -0,0 +1,5 @@
+class_name TheCafe
+extends Node2D
+
+func _ready() -> void:
+	y_sort_enabled = true
