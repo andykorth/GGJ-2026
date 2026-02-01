74862feb4f08bd9ac29ba0aeeb38fc8af62cbea0
Stephen <leonyx03@gmail.com>
2026-01-31 22:00:38 -0600

    Character sighs, fail sound, Lena intro right at start.

--- DialogicFiles/DialogManager.gd
+++ DialogicFiles/DialogManager.gd
@@ -8,6 +8,7 @@ func _ready():
 	Dialogic.timeline_started.connect(GlobalGameVariables.deactivatePlayerControls)
 	Dialogic.timeline_ended.connect(GlobalGameVariables.activatePlayerControls)
 	Dialogic.Text.about_to_show_text.connect(GlobalGameVariables.incrementDialogStepsTaken)
+	DialogicConnector.startDialogue(_lenaCharacter.intro_timeline, _lenaCharacter)
 	
 func _process(delta):
 	pass
